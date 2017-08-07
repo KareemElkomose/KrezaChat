be59853d5f8a5e09821bc0a0ed7084ccd61eb1c5
KareemElkomose <kareem.elkomose@gmail.com>
2017-08-07 14:57:49 +0200

    First commit

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 var port = 5000 || process.env.PORT ;
 
-app.get('/', function(req, res) {
-  res.render(views/index.html); 
-});
+
 
 
 app.listen(port); 
