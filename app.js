14cbc76304b5e493a729947d2ab62b5bfdfe4f2c
Kareem Joseph <30794608+KareemElkomose@users.noreply.github.com>
2017-08-07 15:25:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var app = express();
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 app.get('/', function(req, res) {
   res.render(views/index); 
 });
-var port = 5000 || process.env.PORT ;
 
 
-
-
-app.listen(port); 
