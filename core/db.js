d369cef0a3301c859165fa471af91edbb2216e0f
KareemElkomose <kareem.elkomose@gmail.com>
2017-08-09 16:01:26 +0200

    query

--- core/db.js
+++ core/db.js
@@ -7,6 +7,7 @@ var con = mysql.createConnection({
     port:"3306", 
     username: "sql12189144",
     password: "mHryp22AL9",
+    database: "sql12189144"
     
 //sss
 });
@@ -18,5 +19,18 @@ if(err){
 }
 else console.log("Connected!");
 });
+
+con.query("SELECT * FROM user user" , function (err, result , fields){
+
+    if(err){
+        throw err;
+    }
+
+    console.log(result);
+
+
+});
+
+
 
 module.exports.con;
