b7aee89d4131abc382cff4479eb5f09aa90f53a8
Kareem Joseph <30794608+KareemElkomose@users.noreply.github.com>
2017-08-11 01:41:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();var bodyParser = require('body-parser');
 var jwt= require('jsonwebtoken'); 
 
 var app = express();
-//var database = require('./core/db.js');
+var database = require('./core/db.js');
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
